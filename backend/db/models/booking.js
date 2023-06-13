5b1776d0c403d1f2ad3122162fdf30d4d1e2a0bf
jason murphy <jmurphy.1196@gmail.com>
2023-06-12 21:27:57 -0700

    added booking model

---
+++ backend/db/models/booking.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+const { User } = require("./user");
+const { Spot } = require("./spot");
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init(
+    {
+      spotId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Spots",
+          key: "id",
+        },
+      },
+      userId: DataTypes.INTEGER,
+      startDate: DataTypes.DATE,
+      endDate: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Booking",
+    }
+  );
+  return Booking;
+};
