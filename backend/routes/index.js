36fc55b287a5db3a3af973a0c1e19b667e04d716
jason murphy <jmurphy.1196@gmail.com>
2023-06-30 13:57:58 -0700

    added dist

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
     );
   });
 
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
     );
   });
 }
