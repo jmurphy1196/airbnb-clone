c86a8da6152677554376feb5f6bdc6d329ef6cf5
jason murphy <jmurphy.1196@gmail.com>
2023-06-30 14:06:29 -0700

    added baseurl for prod :

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const ASSET_URL = process.env.BASE_URL || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +11,5 @@ export default defineConfig({
       "/api": "http://localhost:8000",
     },
   },
+  base: ASSET_URL !== "" ? `${ASSET_URL}/frontend/dist` : "",
 });
